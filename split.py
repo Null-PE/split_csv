175b386df322d5cb8401cb88e9345c532ea9b396
NULLPE <1989leosun@gmail.com>
2021-10-01 20:41:45 +0900

    update

--- split.py
+++ split.py
@@ -1,15 +1,26 @@
 import os
+from pathlib import Path
+
 
 def split(filehandler, encoding="CP932", output_path='output', keep_headers=True):
+    subpath = f"{output_path}/{filehandler.split('.')[0]}"
+    Path(subpath).mkdir(parents=True, exist_ok=True)
     file = open(filehandler, encoding=encoding)
     lines = file.readlines()
     subfile = None
+    count = 0
+    if lines:
+        print(f"read {file.name} succeed.")
     for line in lines:
-        if "【select" in line:
+        if "【select" in line.lower():
             if subfile:
                 subfile.close()
-            name = line.split('】',1)[1]
-            subfile = open(f"{output_path}/{name}.csv", "w", encoding="utf8")
+            name = line.strip().split('】', 1)[1]
+            if name is None or 'dummy' in name:
+                name = f"{count}_{line.strip()}"
+                count = count + 1
+            print(f"write into subfile {subpath}/{name}.csv")
+            subfile = open(f"{subpath}/{name}.csv", "w", encoding="utf8")
         elif subfile:
             subfile.write(line)
     if subfile:
@@ -19,5 +30,8 @@ def split(filehandler, encoding="CP932", output_path='output', keep_headers=True
 if __name__ == "__main__":
     files = os.listdir("input")
     for file in files:
+        if file == '.gitkeep':
+            continue
         if file is not os.path.isdir(file):
-            split(os.path.join("input",file))
+            print(f"Split file: {file}")
+            split(os.path.join("input", file))
